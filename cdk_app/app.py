c9e1e2095a2bf0288da23d3353b43a73a1f3df78
Marko Z Mandic <marko.mandic@mmandic.levi9.com>
2022-10-12 11:33:39 +0200

    ready to try gh actions on dev

--- cdk_app/app.py
+++ cdk_app/app.py
@@ -5,13 +5,17 @@ from aws_cdk import (
     Environment,
     App
 )
+from dotenv import load_dotenv
+
+config = load_dotenv('.env')
+environment = config['ENV']
 
 
 from stacks.cdk_app_stack import BussinesLogicStack
 
 
 app = cdk.App()
-BussinesLogicStack(app,'{0}-stack-rekognition'.format(os.getenv('ENVIRONMENT')),
+BussinesLogicStack(app,'{0}-stack-rekognition'.format(environment),
             env=Environment(account=os.getenv('AWS_ACCOUNT'),
                             region=os.getenv('AWS_REGION')))
 
