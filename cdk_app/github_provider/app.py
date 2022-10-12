0c2979f99dd5dee7d56b1ca80decdd3cb13ff0a1
Marko Z Mandic <marko.mandic@mmandic.levi9.com>
2022-10-12 11:23:52 +0200

    openIdConnectProvider implemented

--- cdk_app/github_provider/app.py
+++ cdk_app/github_provider/app.py
@@ -6,7 +6,7 @@ from aws_cdk import (
     App
 )
 
-from github_provider.github_stack.github_stack import AwsGithubActionsPolicyStack
+from github_stack.github_stack import AwsGithubActionsPolicyStack
 load_dotenv()
 
 environment = os.getenv('ENVIRONMENT')
@@ -15,7 +15,7 @@ app_name = os.getenv('APP_NAME')
 
 app = App()
 
-AwsGithubActionsPolicyStack(app,'{0}-stack-github'.format(os.getenv('ENVIRONMENT')),
+AwsGithubActionsPolicyStack(app,'{0}-stack-github'.format(environment),
             env=Environment(account=os.getenv('AWS_ACCOUNT'),
                             region=os.getenv('AWS_REGION')))
 
